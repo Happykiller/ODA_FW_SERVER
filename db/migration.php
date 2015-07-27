cba9c97e3c189a6f152efa8a17ea55e26ff69011
happykiller <fabrice.rosito@gmail.com>
2015-07-27 17:16:22 +0200

    work refresh model

--- db/migration.php
+++ db/migration.php
@@ -18,6 +18,12 @@ $longopts  = array(
 );
 $options = getopt($shortopts, $longopts);
 
+$config = OdaConfig::getInstance();
+
+$params_bd = new stdClass();
+$params_bd->bd_conf = $config->BD_ENGINE;
+$BD_ENGINE = new OdaLibBd($params_bd);
+
 if (!isset($options['target']) ) {
     print "There was a problem reading in the options." . PHP_EOL;
     exit(1);
@@ -33,25 +39,20 @@ if (!isset($options['partial']) ) {
 }
 
 if($options['partial'] !== "all"){
-    exe('./'.$options['target'].'/'.$options['partial'].'/'.$options['option'].'.sql');
+    exe($config, $BD_ENGINE, './'.$options['target'].'/'.$options['partial'].'/'.$options['option'].'.sql', $options);
 }else{
     $objects = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator('./'.$options['target'].'/', \RecursiveDirectoryIterator::SKIP_DOTS), \RecursiveIteratorIterator::SELF_FIRST);
     foreach($objects as $name => $object){
         if ($object->isDir()) {
-            exe($name.'/'.$options['option'].'.sql');
+            $name = str_replace('\\','/',$name);
+            exe($config, $BD_ENGINE, $name.'/'.$options['option'].'.sql', $options);
         }
     }
 }
 
 echo 'Sucess' . PHP_EOL;
 
-function exe($file){
-    $config = OdaConfig::getInstance();
-
-    $params_bd = new stdClass();
-    $params_bd->bd_conf = $config->BD_ENGINE;
-    $BD_ENGINE = new OdaLibBd($params_bd);
-
+function exe($config, $BD_ENGINE, $file, $options){
     echo "Script selected : ". $file . PHP_EOL;
 
     $contentScript = file_get_contents($file, FILE_USE_INCLUDE_PATH);
@@ -63,15 +64,28 @@ function exe($file){
     $params->typeSQL = OdaLibBd::SQL_SCRIPT;
     $retour = $BD_ENGINE->reqODASQL($params);
 
-    echo "Statut : " . $retour->strStatut . (($retour->strStatut != 5) ? (" (error : " . $retour->strErreur . ")") : "")    . PHP_EOL;
-
-    $params = new OdaPrepareReqSql();
-    $params->sql = "
-        INSERT INTO `".$config->BD_ENGINE->prefixTable."api_tab_migration`
-        (`name`, `dateMigration`)
-        VALUES
-        ('".$file."', NOW)
-    ";
-    $params->typeSQL = OdaLibBd::SQL_SCRIPT;
-    $retour = $BD_ENGINE->reqODASQL($params);
+    echo "Status for the migration : " . $retour->strStatut . (($retour->strStatut != 5) ? (" (error : " . $retour->strErreur . ")") : "")    . PHP_EOL;
+    if($options['option'] == "do"){
+        $params = new OdaPrepareReqSql();
+        $params->sql = "
+            INSERT INTO `".$config->BD_ENGINE->prefixTable."api_tab_migration`
+            (`name`, `dateMigration`)
+            VALUES
+            ('".$file."', NOW())
+        ";
+        $params->typeSQL = OdaLibBd::SQL_SCRIPT;
+        $retour = $BD_ENGINE->reqODASQL($params);
+        echo "Status for the trace record : " . $retour->strStatut . (($retour->strStatut != 5) ? (" (error : " . $retour->strErreur . ")") : "")    . PHP_EOL;
+    }elseif($options['option'] == "unDo"){
+        $params = new OdaPrepareReqSql();
+        $file = str_replace('unDo','do',$file);
+        $params->sql = "
+            DELETE FROM `".$config->BD_ENGINE->prefixTable."api_tab_migration`
+            WHERE 1=1
+            AND `name` = '".$file."'
+        ";
+        $params->typeSQL = OdaLibBd::SQL_SCRIPT;
+        $retour = $BD_ENGINE->reqODASQL($params);
+        echo "Status for the trace record : " . $retour->strStatut . (($retour->strStatut != 5) ? (" (error : " . $retour->strErreur . ")") : "")    . PHP_EOL;
+    }
 }
