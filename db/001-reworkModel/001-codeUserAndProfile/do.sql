7d305776886af5f3ca141ce9f62f8a715175ab85
happykiller <fabrice.rosito@gmail.com>
2015-08-28 11:49:03 +0200

    work tool db

--- db/001-reworkModel/001-codeUserAndProfile/do.sql
+++ db/001-reworkModel/001-codeUserAndProfile/do.sql
@@ -2,6 +2,8 @@ ALTER TABLE `@prefix@api_tab_utilisateurs` DROP `login`;
 
 ALTER TABLE `@prefix@api_tab_utilisateurs` ADD `id_rang` INT(10) NOT NULL AFTER `prenom`;
 
+ALTER TABLE `@prefix@api_tab_utilisateurs` ADD `langue` varchar(50) NOT NULL DEFAULT 'fr' AFTER `theme`;
+
 UPDATE `@prefix@api_tab_utilisateurs` a
   JOIN `@prefix@api_tab_rangs` b ON 1=1 AND a.`profile` = b.`indice`
 SET a.`id_rang` = b.`id`
