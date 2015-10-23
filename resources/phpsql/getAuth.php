e1433451875648dff9f14055cfc58b7f0a4992e3
happykiller <fabrice.rosito@gmail.com>
2015-10-23 16:25:22 +0200

    fixe getAuth sur user inconnu

--- resources/phpsql/getAuth.php
+++ resources/phpsql/getAuth.php
@@ -40,11 +40,11 @@ $params = new SimpleObject\OdaPrepareReqSql();
 $params->sql = "select a.`id_rang`, a.`code_user`
     from `api_tab_utilisateurs` a
     where 1=1 
-    and a.`code_user` = :login
+    and a.`code_user` = :code_user
     and a.`password` = :mdp
 ;";
 $params->bindsValue = [
-    "login" => $ODA_INTERFACE->inputs["login"]
+    "code_user" => $ODA_INTERFACE->inputs["login"]
     , "mdp" => $ODA_INTERFACE->inputs["mdp"]
 ];
 $params->typeSQL = OdaLibBd::SQL_GET_ONE;
@@ -52,7 +52,7 @@ $retour = $ODA_INTERFACE->BD_ENGINE->reqODASQL($params);
 
 //--------------------------------------------------------------------------
 //get key
-$key = $ODA_INTERFACE->buildSession(array('code_user' => $retour->data->code_user, 'password' => $ODA_INTERFACE->inputs["mdp"]));
+$key = $ODA_INTERFACE->buildSession(array('code_user' => $ODA_INTERFACE->inputs["login"], 'password' => $ODA_INTERFACE->inputs["mdp"]));
 
 $retour->data->keyAuthODA = $key;
 
