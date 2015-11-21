96762ad7c3a6560ac16b1fd7f93cdb04e7a9faec
Rosito Fabrice <fabrice.rosito@gmail.com>
2015-11-21 18:52:13 +0100

    add rest impl

---
+++ resources/api/rest/index.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Oda;
+
+require '../../header.php';
+require "../../vendor/autoload.php";
+require '../../include/config.php';
+
+use \stdClass, \Oda\SimpleObject\OdaPrepareInterface, \Oda\SimpleObject\OdaPrepareReqSql, \Oda\OdaLibBd;
+
+//--------------------------------------------------------------------------
+//Build the interface
+$params = new OdaPrepareInterface();
+$INTERFACE = new OdaLibInterface($params);
+$app = new \Slim\Slim();
+
+$odaOffset = $app->request->params('odaOffset');
+if(is_null($odaOffset)){
+    $odaOffset = 0;
+}else{
+    $odaOffset = intval($odaOffset);
+}
+$odaLimit = $app->request->params('odaLimit');
+if(is_null($odaLimit)){
+    $odaLimit = 9999;
+}else{
+    $odaLimit = intval($odaLimit);
+}
+
+$app->get('/entity/:id', function ($id) use ($INTERFACE, $odaOffset, $odaLimit) {
+    $INTERFACE->addDataStr("HelloWorld");
+});
+
+$app->run();
