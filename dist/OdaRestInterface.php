0e6db3fa8a455613d4d5cc6266308197cf743a3d
happykiller <fabrice.rosito@gmail.com>
2015-12-04 09:26:28 +0100

    add OdaRestInterface

---
+++ dist/OdaRestInterface.php
@@ -0,0 +1,43 @@
+<?php
+namespace Oda;
+
+use \stdClass, \Oda\OdaLibInterface;
+
+/**
+ * Project class
+ *
+ * Tool
+ *
+ * @author  Fabrice Rosito <rosito.fabrice@gmail.com>
+ * @version 0.150221
+ */
+class OdaRestInterface extends OdaLibInterface {
+    
+    public $odaOffset;
+    
+    public $odaLimit;
+
+    public $slim;
+    
+    public function __construct($params) {
+
+        $_GET = $params->slim->request()->params();
+
+        $this->slim = $params->slim;
+
+        $this->odaOffset = $params->slim->request->params('odaOffset');
+        if(is_null($this->odaOffset)){
+            $this->odaOffset = 0;
+        }else{
+            $this->odaOffset = intval($this->odaOffset);
+        }
+        $this->odaLimit = $params->slim->request->params('odaLimit');
+        if(is_null($this->odaLimit)){
+            $this->odaLimit = 9999;
+        }else{
+            $this->odaLimit = intval($this->odaLimit);
+        }
+        
+        parent::__construct($params);
+    }
+}
