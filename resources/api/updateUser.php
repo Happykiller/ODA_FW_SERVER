20f760b7446f99e9fef0a534b7950b2138377026
happykiller <fabrice.rosito@gmail.com>
2015-12-09 17:30:50 +0100

    update

--- resources/api/updateUser.php
+++ resources/api/updateUser.php
@@ -10,25 +10,8 @@ use \stdClass, \Oda\SimpleObject\OdaPrepareInterface, \Oda\SimpleObject\OdaPrepa
 //--------------------------------------------------------------------------
 //Build the interface
 $params = new SimpleObject\OdaPrepareInterface();
-$params->arrayInput = array("mail","actif", "rang", "code_user");
+$params->arrayInput = array("mail","actif", "rang", "code_user", "desc");
 $ODA_INTERFACE = new OdaLibInterface($params);
-
-//--------------------------------------------------------------------------
-// vendor/happykiller/oda/resources/api/updateUser.php?milis=123450&code_user=VIS&mail=vis.vis@gmail.com&actif=1&rang=10
-
-//--------------------------------------------------------------------------
-$params = new SimpleObject\OdaPrepareReqSql();
-$params->sql = "SELECT a.`id`
-    FROM  `api_tab_rangs` a
-    WHERE 1=1
-    AND a.`indice` = :rang
-;";
-$params->bindsValue = [
-    "rang" => $ODA_INTERFACE->inputs["rang"]
-];
-$params->typeSQL = OdaLibBd::SQL_GET_ONE;
-$retour = $ODA_INTERFACE->BD_ENGINE->reqODASQL($params);
-$id_rang = $retour->data->id;
 
 //--------------------------------------------------------------------------
 $params = new SimpleObject\OdaPrepareReqSql();
@@ -36,6 +19,7 @@ $params->sql = "UPDATE `api_tab_utilisateurs`
     SET `mail` = :mail,
     `actif` = :actif,
     `id_rang` = :rang,
+    `description` = :desc,
     `date_modif` = NOW()
     WHERE 1=1
     AND `code_user` = :code_user
@@ -45,7 +29,8 @@ $params->bindsValue = [
     "code_user" => $ODA_INTERFACE->inputs["code_user"],
     "mail" => $ODA_INTERFACE->inputs["mail"],
     "actif" => $ODA_INTERFACE->inputs["actif"],
-    "rang" => $id_rang
+    "rang" => $ODA_INTERFACE->inputs["rang"],
+    "desc" => $ODA_INTERFACE->inputs["desc"],
 ];
 $params->typeSQL = OdaLibBd::SQL_SCRIPT;
 $retour = $ODA_INTERFACE->BD_ENGINE->reqODASQL($params);
