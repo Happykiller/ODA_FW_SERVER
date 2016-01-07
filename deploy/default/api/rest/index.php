a60c87cdcc3345e4c45190b6767116d6365867ba
happykiller <fabrice.rosito@gmail.com>
2016-01-07 09:29:25 +0100

    update

--- deploy/default/api/rest/index.php
+++ deploy/default/api/rest/index.php
@@ -9,12 +9,14 @@ require '../../include/config.php';
 use cebe\markdown\GithubMarkdown;
 use Slim\Slim;
 use \stdClass, \Oda\SimpleObject\OdaPrepareInterface, \Oda\SimpleObject\OdaPrepareReqSql, \Oda\OdaLibBd;
+use Oda\OdaRestInterface;
 
 $slim = new Slim();
 //--------------------------------------------------------------------------
 
-$slim->notFound(function () {
+$slim->notFound(function () use ($slim) {
     $params = new OdaPrepareInterface();
+    $params->slim = $slim;
     $INTERFACE = new OdaRestInterface($params);
     $INTERFACE->dieInError('not found');
 });
