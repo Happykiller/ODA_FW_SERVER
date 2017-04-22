a93122d99d20d2ee6644b6cae3f36a1e2dd34dc0
Fabrice Rosito <fabrice.rosito@gmail.com>
2017-04-22 14:44:40 +0200

    update

--- dist/InterfaceRest/NavigationInterface.php
+++ dist/InterfaceRest/NavigationInterface.php
@@ -55,6 +55,45 @@ class NavigationInterface extends OdaRestInterface {
     }
     /**
      */
+    function getRight() {
+        try {
+            $filterRank = "";
+            if($this->user->indice != "1"){
+                $params = new OdaPrepareReqSql();
+                $params->sql = "SELECT a.`id`
+                    FROM  `api_tab_rangs` a
+                    WHERE 1=1
+                    AND a.`indice` = :rang
+                ;";
+                $params->bindsValue = [
+                    "rang" => $this->user->indice
+                ];
+                $params->typeSQL = OdaLibBd::SQL_GET_ONE;
+                $retour = $this->BD_ENGINE->reqODASQL($params);
+                $id_rang = $retour->data->id;
+                $filterRank = "AND EXISTS (SELECT 1 FROM `api_tab_menu_rangs_droit` c WHERE c.`id_rang` = ".$id_rang." and c.`id_menu` like CONCAT('%;',a.`id`,';%'))";
+            }
+
+            $params = new OdaPrepareReqSql();
+            $params->sql = "SELECT a.`id`, a.`Description` as 'Description_menu', a.`Description_courte`, a.`Lien`, a.`id_categorie`, b.`Description` as 'Description_cate'
+                FROM  `api_tab_menu` a, `api_tab_menu_categorie` b
+                WHERE 1=1
+                AND a.`id_categorie` = b.id
+                ".$filterRank."
+                ORDER BY a.`id_categorie` asc, a.`Description_courte` asc
+            ;";
+            $params->typeSQL = OdaLibBd::SQL_GET_ALL;
+            $retour = $this->BD_ENGINE->reqODASQL($params);
+
+            $params = new stdClass();
+            $params->retourSql = $retour;
+            $this->addDataObject($retour->data->data);
+        } catch (Exception $ex) {
+            $this->dieInError($ex.'');
+        }
+    }
+    /**
+     */
     function getRights() {
         try {
             $params = new OdaPrepareReqSql();
