0c314371ef939e33f5df601729ee9279db94a404
Fabrice Rosito <fabrice.rosito@gmail.com>
2017-04-22 14:34:45 +0200

    update

---
+++ db/009-170422/001-navigation/do.sql
@@ -0,0 +1,5 @@
+SET FOREIGN_KEY_CHECKS=0;
+-- --------------------------------------------------------
+INSERT INTO `@prefix@api_tab_menu` (`Description`, `Description_courte`, `id_categorie`, `Lien`) VALUES ('oda-navigation.title', 'oda-tests.navigation', 2, 'navigation');
+-- --------------------------------------------------------
+SET FOREIGN_KEY_CHECKS=1;
